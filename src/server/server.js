493b5f5dd14e3adf99670dd02e940a1a53068053
FredLitt <80988264+FredLitt@users.noreply.github.com>
2022-03-08 21:12:12 +0000

    added sockets to server

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,53 @@
-const express = require('express')
-const app = express()
-const port = 3001
-
+const path = require('path');
+const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
 const Client = require("@replit/database");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+async function initializeGame(client) {
+  let initialGame = await client.get("game");
+  if (initialGame === null){
+    await client.set("game", [])
+  }
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+async function startGame() {
+  const client = new Client();
+  await initializeGame(client)
 
-const client = new Client()
+  const publicPath = path.join(__dirname, './public');
+  const port = process.env.PORT || 3001;
+  let app = express();
+  let server = http.createServer(app);
+  let io = socketIO(server);
+  
+  app.use(express.static(publicPath))
+  
+  server.listen(port, () => {
+      console.log(`Server is up on port ${port}.`)
+  });
+  
+  io.on("connection", async (socket) => {
+      console.log("A user just connected.");
+      socket.on("disconnect", () => {
+          console.log("A user has disconnected.");
+      })
+      socket.on("movePawn", async () => {
+          let number = await client.get("game")
+          const newMove = { 
+            fromSquare: [1, 3], 
+            toSquare: [3, 3]
+          }
+          const currentGame = await client.get("game")
+          const updatedGame = [...currentGame, newMove]
+        
+          await client.set("game", )
+          io.emit("updatedNumber", newNumber)
+      })
+      const currentGame = await client.get("game")
+      socket.emit("updatedNumber", currentNumber)
+  });
+}
+
+startGame()
+
