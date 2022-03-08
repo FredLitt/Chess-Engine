f21a19e571c6f8a60215c4ca7f39801df05e7d32
FredLitt <80988264+FredLitt@users.noreply.github.com>
2022-03-08 20:40:42 +0000

    defined pieces on backend

---
+++ src/server/pieces.js
@@ -0,0 +1,17 @@
+// This will be similar to front end start position but the pieces will be represented by simple objects rather than classes
+
+const pieces = {
+  whitePawn: { type: "pawn", color: "white"},
+  whiteKnight: { type: "knight", color: "white" },
+  whiteBishop: { type: "bishop", color: "white" },
+  whiteRook: { type: "rook", color: "white" },
+  whiteQueen: { type: "queen", color: "white" },
+  whiteKing: { type: "king", color: "white" },
+  
+  blackPawn : { type: "pawn", color: "black" },
+  blackKnight : { type: "knight", color: "black" },
+  blackBishop : { type: "bishop", color: "black" },
+  blackRook : { type: "rook", color: "black" },
+  blackQueen : { type: "queen", color: "black" },
+  blackKing : { type: "king", color: "black" }
+}
