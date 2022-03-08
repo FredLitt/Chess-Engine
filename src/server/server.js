6a13a8b8d0634c47375159336d934a95da878ba7
FredLitt <80988264+FredLitt@users.noreply.github.com>
2022-03-08 20:57:40 +0000

    added public + server folders

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,16 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const port = 3001
-const socketIO = require("socket.io")
-const Client = require("@replit/database")
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
+const socketIO = require('socket.io');
+const Client = require("@replit/database");
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-async function initializeGame(client){
-  let 
-}
+const client = new Client()
